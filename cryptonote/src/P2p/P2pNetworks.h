10b80840b9cdea38e3ad47b4e0a3d834163e69d7
Codex02 <32749284+Codex02@users.noreply.github.com>
2017-10-17 22:24:30 -0400

    Source

---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x01, 0xB4, 0xbb, 0xcc, 0x5F, 0xC4, 0xEA, 0xDE, 0x3D, 0x9F, 0x4D, 0x6A, 0x66, 0x5C, 0xC7, 0x11 } };
+}
